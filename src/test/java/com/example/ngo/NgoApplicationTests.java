bf0fee6da8f889357373519716f3be5ecf152f4e
suhas177 <400suhas96@gmail.com>
2024-01-19 16:47:58 +0530

    initial commit

---
+++ src/test/java/com/example/ngo/NgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
